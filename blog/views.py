c15494135c4c62ca7837a9bd8cac7896ece9b5ec
Sam Juhyun Kang <kjh91.sam@gmail.com>
2020-09-03 18:28:35 +0900

    learned how to make template tags

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
+from django.utils import timezone
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('published_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+
+
